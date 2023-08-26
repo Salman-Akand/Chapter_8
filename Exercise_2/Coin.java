5c8edd1d384f1e259c8c7a1c86566e7ffc696938
Salman-Akand <salmanakand5678@gmail.com>
2023-08-26 18:23:36 +0600

    Commit_2

---
+++ Exercise_2/Coin.java
@@ -0,0 +1,17 @@
+public class Coin {
+    private double coinValue;
+    private String coinName;
+
+    public Coin(double value, String name) {
+        coinValue = value;
+        coinName = name;
+    }
+
+    public double getCoinValue() {
+        return coinValue;
+    }
+
+    public String getCoinName() {
+        return coinName;
+    }
+}
